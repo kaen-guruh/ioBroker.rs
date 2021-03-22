4c61dc2adc8dc7c6273ff0d54d238520ffe00277
Kaenguruh <kaen-guruh@web.de>
2021-03-22 09:32:42 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'rs adapter settings': {
+		'en': 'Adapter settings for rs',
+		'de': 'Adaptereinstellungen für rs',
+		'ru': 'Настройки адаптера для rs',
+		'pt': 'Configurações do adaptador para rs',
+		'nl': 'Adapterinstellingen voor rs',
+		'fr': "Paramètres d'adaptateur pour rs",
+		'it': "Impostazioni dell'adattatore per rs",
+		'es': 'Ajustes del adaptador para rs',
+		'pl': 'Ustawienia adaptera dla rs',
+		'zh-cn': 'rs的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
